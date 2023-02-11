2d03851954979867c745051c0477fd7df635bf88
sscademy <sscademy@gmail.com>
2023-02-11 23:37:27 +0530

    created new branch

--- website.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install apache2 wget unzip -y
-wget https://www.tooplate.com/zip-templates/2122_nano_folio.zip
-unzip 2122_nano_folio.zip
-cp -r 2122_nano_folio/* /var/www/html/
-systemctl restart apache2
